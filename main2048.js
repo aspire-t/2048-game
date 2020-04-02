4523bc556177cc0a3d25b3ddd123a3807e35ea42
aspire-t <zhijianwubu@gmail.com>
2020-04-02 18:48:26 +0800

    init number

--- main2048.js
+++ main2048.js
@@ -3,26 +3,77 @@
  * my site: http://www.liuyubobobo.com
  */
 
-var board = new Array();
-var score = 0;
+var board = new Array()
+var score = 0
+var hasConflicted = new Array()
 
-$(document).ready(function(){
-    newgame();
-});
+var startx = 0
+var starty = 0
+var endx = 0
+var endy = 0
 
-function newgame(){
-    //初始化棋盘格
-    init();
-    //在随机两个格子生成数字
+$(document).ready(function() {
+  newGame()
+})
+
+function newGame() {
+  // 初始化棋盘格
+  init()
+  //在随机两个格子生成数字
+  generateOneNumber()
+  generateOneNumber()
 }
 
-function init(){
-    for( var i = 0 ; i < 4 ; i ++ )
-        for( var j = 0 ; j < 4 ; j ++ ){
+function init() {
+  // 修改每个cell的样式
+  for (var i = 0; i < 4; i++) {
+    for (var j = 0; j < 4; j++) {
+      var gridCell = $('#grid-cell-' + i + '-' + j)
+      gridCell.css('top', getPosTop(i, j))
+      gridCell.css('left', getPosLeft(i, j))
+    }
+  }
+  // 先将board 置为二维数组
+  for (let i = 0; i < 4; i++) {
+    board[i] = new Array()
+    for (let j = 0; j < 4; j++) {
+      board[i][j] = 0
+    }
+  }
 
-            var gridCell = $('#grid-cell-'+i+"-"+j);
-            gridCell.css('top', getPosTop( i , j ) );
-            gridCell.css('left', getPosLeft( i , j ) );
-        }
+  updateBoardView()
+}
 
+function updateBoardView() {
+  $('.number-cell').remove()
+
+  for (var i = 0; i < 4; i++) {
+    for (var j = 0; j < 4; j++) {
+      $('#grid-container').append(
+        '<div class="number-cell" id="number-cell-' + i + '-' + j + '"></div>'
+      )
+      var theNumberCell = $('#number-cell-' + i + '-' + j)
+
+      if (board[i][j] == 0) {
+        // 初始化 numberCell 的样式，开始的时候 width 和 height 都是 0，
+        theNumberCell.css('width', '0px')
+        theNumberCell.css('height', '0px')
+        // 将 numberCell 放到每个 cell 的中间，getPosTop 获取到的值，就是 numberCell 左上角的值
+        theNumberCell.css('top', getPosTop(i, j) + 50 / 2)
+        theNumberCell.css('left', getPosLeft(i, j) + 50 / 2)
+      } else {
+        theNumberCell.css('width', '100px')
+        theNumberCell.css('height', '100px')
+        theNumberCell.css('top', getPosTop(i, j))
+        theNumberCell.css('left', getPosLeft(i, j))
+        // 设置样式
+        theNumberCell.css(
+          'background-color',
+          getNumberBackgroundColor(board[i][j])
+        )
+        theNumberCell.css('color', getNumberColor(board[i][j]))
+        theNumberCell.text(board[i][j])
+      }
+    }
+  }
 }
--- support2048.js
+++ support2048.js
@@ -2,11 +2,49 @@
  * Created by liuyubobobo on 14-4-11.
  * my site: http://www.liuyubobobo.com
  */
-function getPosTop( i , j ){
-    return 20 + i*120;
+function getPosTop(i, j) {
+  return 20 + i * 120
 }
 
-function getPosLeft( i , j ){
-    return 20 + j*120;
+function getPosLeft(i, j) {
+  return 20 + j * 120
 }
 
+function getNumberBackgroundColor(number) {
+  switch (number) {
+    case 2:
+      return '#eee4da'
+    case 4:
+      return '#ede0c8'
+    case 8:
+      return '#f2b179'
+    case 16:
+      return '#f59563'
+    case 32:
+      return '#f67c5f'
+    case 64:
+      return '#f65e3b'
+    case 128:
+      return '#edcf72'
+    case 256:
+      return '#edcc61'
+    case 512:
+      return '#9c0'
+    case 1024:
+      return '#33b5e5'
+    case 2048:
+      return '#09c'
+    case 4096:
+      return '#a6c'
+    case 8192:
+      return '#93c'
+  }
+
+  return 'black'
+}
+
+function getNumberColor(number) {
+  if (number <= 4) return '#776e65'
+
+  return 'white'
+}
